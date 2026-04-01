db48666f0b14e68d89ce9e4dd99d1681848960c5
sijafaiza <pijazp@gmail.com>
2026-04-01 12:28:15 +0700

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // GANTI DENGAN DATA DARI SUPABASE ANDA
 const SUPABASE_URL = 'https://yjseswgbuumkhvxywvnc.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlqc2Vzd2didXVta2h2eHl3dm5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMDYxOTMsImV4cCI6MjA4ODY4MjE5M30.jd4tkvZe_tGSgMWtqBIxLSXg97ncoGCDaoALSp-gedo...'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlqc2Vzd2didXVta2h2eHl3dm5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMDYxOTMsImV4cCI6MjA4ODY4MjE5M30.jd4tkvZe_tGSgMWtqBIxLSXg97ncoGCDaoALSp-gedo'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
